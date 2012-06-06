b5cc1af30b51c7da5cae6ffb2212d51c674b0cbc
Henri <henri.kuusirati@gmail.com>
2012-06-06 14:39:00 +0300

    ObjectListViewin testausta.

--- Hauli/ContestantListForm.Designer.cs
+++ Hauli/ContestantListForm.Designer.cs
@@ -31,6 +31,13 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.contestantObjectListView = new BrightIdeasSoftware.ObjectListView();
+            this.round = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn1 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn2 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn3 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn4 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            ((System.ComponentModel.ISupportInitialize)(this.contestantObjectListView)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -48,11 +55,11 @@
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(215, 12);
+            this.textBox1.Location = new System.Drawing.Point(215, 404);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(338, 546);
+            this.textBox1.Size = new System.Drawing.Size(604, 154);
             this.textBox1.TabIndex = 1;
             // 
             // button2
@@ -65,16 +72,77 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // contestantObjectListView
+            // 
+            this.contestantObjectListView.AllColumns.Add(this.round);
+            this.contestantObjectListView.AllColumns.Add(this.olvColumn1);
+            this.contestantObjectListView.AllColumns.Add(this.olvColumn2);
+            this.contestantObjectListView.AllColumns.Add(this.olvColumn3);
+            this.contestantObjectListView.AllColumns.Add(this.olvColumn4);
+            this.contestantObjectListView.AllowDrop = true;
+            this.contestantObjectListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.contestantObjectListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.round,
+            this.olvColumn1,
+            this.olvColumn2,
+            this.olvColumn3,
+            this.olvColumn4});
+            this.contestantObjectListView.FullRowSelect = true;
+            this.contestantObjectListView.HasCollapsibleGroups = false;
+            this.contestantObjectListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.contestantObjectListView.IsSimpleDragSource = true;
+            this.contestantObjectListView.IsSimpleDropSink = true;
+            this.contestantObjectListView.Location = new System.Drawing.Point(215, 12);
+            this.contestantObjectListView.Name = "contestantObjectListView";
+            this.contestantObjectListView.ShowItemCountOnGroups = true;
+            this.contestantObjectListView.Size = new System.Drawing.Size(604, 371);
+            this.contestantObjectListView.SpaceBetweenGroups = 3;
+            this.contestantObjectListView.TabIndex = 3;
+            this.contestantObjectListView.UseCompatibleStateImageBehavior = false;
+            this.contestantObjectListView.View = System.Windows.Forms.View.Details;
+            this.contestantObjectListView.DragDrop += new System.Windows.Forms.DragEventHandler(this.contestantObjectListView_DragDrop);
+            // 
+            // round
+            // 
+            this.round.AspectName = "Round";
+            this.round.IsEditable = false;
+            this.round.IsVisible = false;
+            this.round.Width = 0;
+            // 
+            // olvColumn1
+            // 
+            this.olvColumn1.AspectName = "Name";
+            this.olvColumn1.Sortable = false;
+            // 
+            // olvColumn2
+            // 
+            this.olvColumn2.AspectName = "Occupation";
+            this.olvColumn2.Sortable = false;
+            // 
+            // olvColumn3
+            // 
+            this.olvColumn3.AspectName = "CulinaryRating";
+            this.olvColumn3.Sortable = false;
+            // 
+            // olvColumn4
+            // 
+            this.olvColumn4.AspectName = "YearOfBirth";
+            this.olvColumn4.Sortable = false;
+            // 
             // ContestantListForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(565, 570);
+            this.ClientSize = new System.Drawing.Size(831, 570);
+            this.Controls.Add(this.contestantObjectListView);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "ContestantListForm";
             this.Text = "ContestantListForm";
+            ((System.ComponentModel.ISupportInitialize)(this.contestantObjectListView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,5 +153,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
+        private BrightIdeasSoftware.ObjectListView contestantObjectListView;
+        private BrightIdeasSoftware.OLVColumn olvColumn1;
+        private BrightIdeasSoftware.OLVColumn olvColumn2;
+        private BrightIdeasSoftware.OLVColumn olvColumn3;
+        private BrightIdeasSoftware.OLVColumn olvColumn4;
+        private BrightIdeasSoftware.OLVColumn round;
     }
 }
--- Hauli/ContestantListForm.cs
+++ Hauli/ContestantListForm.cs
@@ -7,17 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Data.SqlServerCe;
+using BrightIdeasSoftware;
 
 namespace Hauli
 {
     public partial class ContestantListForm : Form
     {
         private HauliDBHandler dbHandler;
+        List<Person> masterList;
+        List<Person> list;
 
         public ContestantListForm(HauliDBHandler dbHandler)
         {
             InitializeComponent();
             this.dbHandler = dbHandler;
+
+
+            masterList = new List<Person>();
+            masterList.Add(new Person("Erä 1", "Wilhelm Frat", "Gymnast", 21, new DateTime(1984, 9, 23), 45.67, false, "Aggressive, belligerent "));
+            masterList.Add(new Person("Erä 2", "Alana Roderick", "Gymnast", 21, new DateTime(1974, 9, 23), 245.67, false, "Beautiful, exquisite"));
+            masterList.Add(new Person("Erä 1", "Frank Price", "Dancer", 30, new DateTime(1965, 11, 1), 75.5, false, "Competitive, spirited"));
+            masterList.Add(new Person("Erä 1", "Eric", "Half-a-bee", 1, new DateTime(1966, 10, 12), 12.25, true, "Diminutive, vertically challenged"));
+            masterList.Add(new Person("Erä 3", "Nicola Scotts", "Nurse", 42, new DateTime(1965, 10, 29), 1245.7, false, "Wise, fun, lovely"));
+            masterList.Add(new Person("Erä 3", "Madalene Alright", "School Teacher", 21, new DateTime(1964, 9, 23), 145.67, false, "Extensive, dimensionally challenged"));
+            masterList.Add(new Person("Erä 1", "Ned Peirce", "School Teacher", 21, new DateTime(1960, 1, 23), 145.67, false, "Fulsome, effusive"));
+            masterList.Add(new Person("Erä 2", "Felicity Brown", "Economist", 30, new DateTime(1975, 1, 12), 175.5, false, "Gifted, exceptional"));
+            masterList.Add(new Person("Erä 1", "Urny Unmin", "Economist", 41, new DateTime(1956, 9, 24), 212.25, true, "Heinous, aesthetically challenged"));
+            masterList.Add(new Person("Erä 3", "Terrance Darby", "Singer", 35, new DateTime(1970, 9, 29), 1145, false, "Introverted, relationally challenged"));
+            //masterList.Add(new Person("Mister Null"));
+
+
+            list = new List<Person>();
+            foreach (Person p in masterList)
+                list.Add(new Person(p));
+
+            contestantObjectListView.DragSource = new SimpleDragSource();
+
+            contestantObjectListView.DropSink = new RearrangingDropSink(true);
+
+            contestantObjectListView.SetObjects(list);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -38,7 +66,111 @@ namespace Hauli
 
         private void button2_Click(object sender, EventArgs e)
         {
-            dbHandler.addContestant();
+
+            masterList.Add(new Person("Erä 1", "Pensseli-setä", "Singer", 35, new DateTime(1970, 9, 29), 1145, false, "Introverted, relationally challenged"));
+
+            list = new List<Person>();
+            foreach (Person p in masterList)
+                list.Add(new Person(p));
+
+            contestantObjectListView.SetObjects(list);
         }
+
+        private void contestantObjectListView_DragDrop(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("Drop-event");
+        }
+    }
+
+    class Person
+    {
+        public bool IsActive = true;
+
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public Person(string round, string name, string occupation, int culinaryRating, DateTime birthDate, double hourlyRate, bool canTellJokes, string comments)
+        {
+            this.round = round;
+            this.name = name;
+            this.Occupation = occupation;
+            this.culinaryRating = culinaryRating;
+            this.birthDate = birthDate;
+            this.hourlyRate = hourlyRate;
+            this.CanTellJokes = canTellJokes;
+            this.Comments = comments;
+        }
+
+        public Person(Person other)
+        {
+            this.round = other.Round;
+            this.name = other.Name;
+            this.Occupation = other.Occupation;
+            this.culinaryRating = other.CulinaryRating;
+            this.birthDate = other.BirthDate;
+            this.hourlyRate = other.GetRate();
+            this.CanTellJokes = other.CanTellJokes;
+            this.Comments = other.Comments;
+        }
+
+        // Allows tests for properties.
+        public string Round
+        {
+            get { return round; }
+            set { round = value; }
+        }
+        private string round;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string name;
+
+        public string Occupation
+        {
+            get { return occupation; }
+            set { occupation = value; }
+        }
+        private string occupation;
+
+        public int CulinaryRating
+        {
+            get { return culinaryRating; }
+            set { culinaryRating = value; }
+        }
+        private int culinaryRating;
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+        private DateTime birthDate;
+
+        public int YearOfBirth
+        {
+            get { return this.BirthDate.Year; }
+            set { this.BirthDate = new DateTime(value, birthDate.Month, birthDate.Day); }
+        }
+
+        // Allow tests for methods
+        public double GetRate()
+        {
+            return hourlyRate;
+        }
+        private double hourlyRate;
+
+        public void SetRate(double value)
+        {
+            hourlyRate = value;
+        }
+
+        // Allows tests for fields.
+        public string Comments;
+        public bool? CanTellJokes;
     }
 }

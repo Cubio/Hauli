449c12ce68386560db61d76b0352882adca7a418
Henri <henri.kuusirati@gmail.com>
2012-06-15 14:47:58 +0300

    Listanmuokkausnäkymä toteutettu. Listan järjestyksen tallennus toteutettu.

--- Hauli/ContestantRowEditForm.cs
+++ Hauli/ContestantRowEditForm.cs
@@ -11,14 +11,47 @@ namespace Hauli
 {
     public partial class ContestantRowEditForm : Form
     {
-        ContestantListLine contestant;
+        private Contestant contestant;
+        private Contestant updatedContestant;
+        private ContestantListForm clf;
 
-        public ContestantRowEditForm(ContestantListLine c)
+        public ContestantRowEditForm(ContestantListForm f, Contestant c)
         {
             InitializeComponent();
             contestant = c;
+            clf = f;
+
+            List<string> sarjaList = clf.GetSarjaList();
+            List<string> seuraList = clf.GetSeuraList();
+
+            sarjaComboBox.Items.AddRange(sarjaList.ToArray());
+            seuraComboBox.Items.AddRange(seuraList.ToArray());
+
+            firstNameTextBox.Text = contestant.FirstName;
+            lastNameTextBox.Text = contestant.LastName;
+            joukkueComboBox.Text = contestant.Team;
+            sarjaComboBox.Text = contestant.Sarja;
+            seuraComboBox.Text = contestant.Seura;
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
 
+            Console.WriteLine("Click: " + firstNameTextBox.Text + " " + lastNameTextBox.Text);
+
+            contestant.FirstName = firstNameTextBox.Text;
+            contestant.LastName = lastNameTextBox.Text;
+            contestant.FullName = firstNameTextBox.Text + " " + lastNameTextBox.Text;
+
+            Console.WriteLine("Click 2: " + contestant.FirstName + " " + contestant.LastName + " " + contestant.FullName);
+
+            contestant.Team = joukkueComboBox.Text;
+            contestant.Sarja = sarjaComboBox.Text;
+            contestant.Seura = seuraComboBox.Text;
+
+            clf.UpdateContestantLine(contestant);
+
+            this.Close();
+        }
     }
 }
